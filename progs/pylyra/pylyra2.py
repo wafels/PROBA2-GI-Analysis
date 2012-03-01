b3ec4f176ffea1fd0aae8f49f8d5985a4974d8bd
Jack Ireland <jack.ireland@nasa.gov>
2012-03-01 17:23:11 -0500

    now depends on sunpy (anytim)

--- progs/pylyra/pylyra2.py
+++ progs/pylyra/pylyra2.py
@@ -1,9 +1,40 @@
+"""
+    Provides programs to process and analyze LYRA data. This module is
+    currently in development.
+   
+    Examples
+    --------
+    To make a LYRA plot
+    >>> import sunpy.instr.lyra as lyra
+    >>> lobj = lyra.lyra()
+    >>> lobj.download('2011/07/23')
+    >>> lobj.load()
+    >>> lobj.plot()
+    
+    To Do
+    -----
+    * An object should be developed to encapsulate all of goes functionality
+    * LYRA FITS files store one day's of data at a time.  A simple extension
+        of the code would make it easy download multiple days worth of data
+        at the one time and create very large arrays of data between
+        specific times.
+    
+    See Also
+    --------
+    For current data plots see http://proba2.sidc.be
+    
+    References
+    ----------
+    | http://proba2.sidc.be/index.html/science/lyra-analysis-manual/article/lyra-analysis-manual?menu=32
+
+    """
 
 import pyfits
 import datetime
 from matplotlib import pyplot as plt
 import urllib,os
 from sgmllib import SGMLParser
+from sunpy.util.util import anytim
 
 """
 class used to scrape the webpage
@@ -32,15 +63,18 @@ class lyra:
         self.downloadto = os.path.expanduser('~')+os.sep
         self.location = 'http://proba2.oma.be/lyra/data/bsd/'
         self.prefix = 'lyra_'
+        self.time = None
         
     def download(self,inputTime):
-        """ Function to download LYRA data"""
+        """ Function to download LYRA data, and/or set the filename where it can be found"""
+
+        self.time = anytim(inputTime)
 
         # date-based subdirectory
-        dateLocation = inputTime.strftime('%Y/%m/%d/')
+        dateLocation = self.time.strftime('%Y/%m/%d/')
 
         # date-based filename
-        dateFilename = inputTime.strftime('%Y%m%d-')
+        dateFilename = self.time.strftime('%Y%m%d-')
 
         # extension to the file name
         extension = '000000_lev'+ str(self.level) + '_' + self.dataType + '.fits'
@@ -61,12 +95,14 @@ class lyra:
             self.filename = wgetDownload(requestedLocation,requestedFile, self.downloadto)
 
     def load(self):
+        """Load the FITS file data into the object"""
         hdu = pyfits.open(self.filename)
         self.data = hdu[1].data
         self.columns = hdu[1].columns
         hdu.close()
 
     def plot(self):
+        """Plot the LYRA data"""
         names = self.columns.names
         units = self.columns.units
         time = self.data.field(0)
@@ -81,10 +117,18 @@ class lyra:
         plt.show()
 
     def cut(self,time1,time2):
+        """Returns a subsection of the data based on times"""
         pass
 
-    def sumup(self,nbins = 20):
-        pass
+    def sumup(self,nbins):
+        """Add up the data using a bin size in # bins"""
+        n = self.data.field(0).size
+        nNew = n/nbins
+        dataNew = self.data
+        for i in range(0,nNew):
+            dataNew.field(0)[i] = data.field(0)[i*nNew]
+        
+        self.data = dataNew
 
 #
 # general purpose downloader using wget
--- progs/test/test4.py
+++ progs/test/test4.py
@@ -1,20 +1,11 @@
 import pylyra2
 import datetime
 
-# set up the data you want
-date = datetime.datetime(2011,03,04)
-
 # initiate the object
 lobj = pylyra2.lyra()
 
-# download the data
-lobj.download(date)
-
 # load the data into the hdu
-lobj.load()
+lobj.download('2011/06/29')
 
 # plot the summary
 lobj.plot()
-
-# sum the data
-lobj.sumup(
