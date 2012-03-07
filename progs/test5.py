7783ae602147ff2f9715b240ebe17049c5f8ee2b
Jack Ireland <jack.ireland@nasa.gov>
2012-03-07 08:00:04 -0500

    test5 slightly improved

--- progs/test5.py
+++ progs/test5.py
@@ -1,23 +1,29 @@
 """
 Test code that makes use of the lyra and kPyWavelet code.
 """
-import lyra,lyra_gi
+import lyra,lyra_gi,sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-lobj = lyra.lyra('2011/06/29')
-lobj.download()
-lobj.load()
+def main():
+    lobj = lyra.lyra('2011/06/29')
+    lobj.download()
+    lobj.load()
 
-# 300 seconds worth of data summed up into 4 second bins
-s = lyra.subthensum(lobj,'2011/06/29 20:00',300,binsize = 4.0)
+    # 300 seconds worth of data summed up into 4 second bins
+    s = lyra.subthensum(lobj,'2011/06/29 20:00',300,binsize = 4.0)
 
-channel = lyra.channel(s,4)
+    channel = lyra.channel(s,4)
 
-data = np.ones(shape=(300,),dtype=np.float32)
-data[150]=3.0
-dt = 1.0
+    analyse = lyra_gi.lyra_gi(channel.data, channel.dt)
+    analyse.dj = 0.125
+    analyse.countFlares()
+    plt.figure(1)
+    plt.imshow(analyse.wave.real,aspect = 'auto')
 
-analyse = lyra_gi.lyra_gi(data, dt)
-analyse.dj = 0.125
-analyse.countFlares()
+    analyse.neupert()
+    plt.figure(2)
+    plt.imshow(analyse.wave.real,aspect='auto')
+
+if __name__ == '__main__':
+    sys.exit(main())
